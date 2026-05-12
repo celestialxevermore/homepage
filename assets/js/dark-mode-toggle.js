6b18d789ef5955cc2c3939f995f1ebaba383b152
key <key2317@naver.com>
2026-05-12 13:32:15 +0900

    Force dark mode on by default

--- assets/js/dark-mode-toggle.js
+++ assets/js/dark-mode-toggle.js
@@ -1,15 +1,15 @@
 function toggleDarkMode() {
-    const body = document.body; 
-    body.classList.toggle('dark-mode'); 
-    const isDarkMode = body.classList.contains('dark-mode'); 
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+    const isDarkMode = body.classList.contains('dark-mode');
     localStorage.setItem('darkMode', isDarkMode);
 }
 
-
-document.addEventListener('DOMContentLoaded', (event) =>{
-    const savedMode = localStorage.getItem('darkMode');
-    if (savedMode === null || savedMode === 'true') {
+document.addEventListener('DOMContentLoaded', () => {
+    const saved = localStorage.getItem('darkMode');
+    if (saved === 'false') {
+        document.body.classList.remove('dark-mode');
+    } else {
         document.body.classList.add('dark-mode');
     }
 });
-
